60fd2a2519e23e77c117ec9eabd6a4a944f23fa5
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-03-25 20:30:17 -0300

    'fim'

--- users/UserController.js
+++ users/UserController.js
@@ -56,7 +56,7 @@ router.post("/authenticate", (req, res) => {
                     id: user.id,
                     email: user.email
                 }
-                res.json(req.session.user);
+                res.redirect("/admin/articles");
             }else{
                 res.redirect("/login");
             }
@@ -65,5 +65,10 @@ router.post("/authenticate", (req, res) => {
         }
     });
 });
+
+router.get("/logout", (req, res) => {
+    req.session.user = undefined;
+    res.redirect("/");
+})
 
 module.exports= router;
